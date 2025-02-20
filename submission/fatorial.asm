f91eb068ab08beeabece11007feee082f96bdec1
Betoh-20 <robertohiraoka@usp.br>
2025-02-20 20:04:05 +0000

    fatorial.mvn v2

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,24 +1,26 @@
 START    SC FATORIAL ; Chama a subrotina FATORIAL
 END      HM /0       ; Halt Machine
 
-FATORIAL RS /0       ; Subrotina FATORIAL
+FATORIAL HM /0       ; Subrotina FATORIAL
 
-         ; Avalia se acabou o fatorial
-         LD N        ; Armazena o valor de N no AC               
-         JZ FATORIAL ; Se AC for igual a 0, retorna a subrotina
+            ; Avalia se acabou o fatorial
+    LOOP    LD N        ; Armazena o valor de N no AC               
+            JZ OUTPUT   ; Se AC for igual a 0, retorna a subrotina
 
-         ; Multiplica o valor de fatorial por N
-         LD RES      ; Armazena o valor de F no AC
-         ML N        ; Multiplica F por N                        
-         MM RES      ; Salva AC em F
+            ; Multiplica o valor de fatorial por N
+            LD RES      ; Armazena o valor de F no AC
+            ML N        ; Multiplica F por N                        
+            MM RES      ; Salva AC em F
 
-         ; Diminui N em 1
-         LD N        ; Armazena o valor de N no AC
-         SB UM       ; Decrementa AC em 1
-         MM N        ; Salva AC em N
+            ; Diminui N em 1
+            LD N        ; Armazena o valor de N no AC
+            SB UM       ; Decrementa AC em 1
+            MM N        ; Salva AC em N
+            JP LOOP     ; Reinicia a subrotina
 
-         JP FATORIAL ; Reinicia a subrotina
+    OUTPUT  RS FATORIAL ; Retorna a subrotina
 
-N        K /3        ; Variavel N
+         @ /100
+N        K /0        ; Variavel N
 RES      K /1        ; Fatorial
 UM       K /1        ; Decremento em 1
